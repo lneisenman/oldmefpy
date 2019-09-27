f3dc32188b08ed7d5cd12539f7d4136b1a66d07f
Larry Eisenman <leisenman@wustl.edu>
2019-09-27 09:07:49 -0500

    update __init__.py

--- oldmefpy/__init__.py
+++ oldmefpy/__init__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import (print_function, absolute_import,
-                        division, unicode_literals)
-
 __author__ = 'Larry Eisenman'
 __email__ = 'leisenman@wustl.edu'
 __version__ = '0.1.0'
 
-from .oldmefpy import helloworld
+from .oldmefpy import read_metadata, read_mef
